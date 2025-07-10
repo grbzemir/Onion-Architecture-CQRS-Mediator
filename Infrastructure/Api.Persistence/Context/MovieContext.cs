882f5d553c90a5d94ad34ba6913a3425e3e2b4e1
grbzemir <eyetkingurbuz@trakya.edu.tr>
2025-07-10 17:26:43 +0300

    DbContext

---
+++ Infrastructure/Api.Persistence/Context/MovieContext.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Api.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace Api.Persistence.Context
+{
+    public class MovieContext:DbContext
+    {
+        public MovieContext(DbContextOptions<MovieContext> options) : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=EMIR\\SQLEXPRESS;Database=MovieDb;TrustServerCertificate=True;");
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+        public DbSet<Cast> Casts { get; set; }
+
+    }
+}
