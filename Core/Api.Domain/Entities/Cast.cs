8fc886423555d4e25bc4e6ad89d20bac007936e2
grbzemir <eyetkingurbuz@trakya.edu.tr>
2025-07-10 16:52:14 +0300

    Models

--- Core/Api.Domain/Entities/Cast.cs
+++ Core/Api.Domain/Entities/Cast.cs
@@ -11,6 +11,10 @@ namespace Api.Domain.Entities
         public  int CastId { get; set; }
         public string Title { get; set; }
         public string Name { get; set; }
+        public string Surname { get; set; }
         public string ImageUrl { get; set; }
+        public string? Overview { get; set; }
+        public string? Biography { get; set; }
+       
     }
 }
