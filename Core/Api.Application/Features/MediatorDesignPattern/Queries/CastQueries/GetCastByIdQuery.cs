104aed58f9e3cf3dd882f72ebaa6cf0f0cfb9998
grbzemir <eyetkingurbuz@trakya.edu.tr>
2025-07-11 18:23:24 +0300

    Queries

---
+++ Core/Api.Application/Features/MediatorDesignPattern/Queries/CastQueries/GetCastByIdQuery.cs
@@ -0,0 +1,20 @@
+﻿using Api.Application.Features.MediatorDesignPattern.Results.CastResults;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Api.Application.Features.MediatorDesignPattern.Queries.CastQueries
+{
+    public class GetCastByIdQuery:IRequest<GetCastByIdQueryResult>
+    {
+        public int CastId { get; set; }
+
+        public GetCastByIdQuery(int castId)
+        {
+            CastId = castId;
+        }
+    }
+}
---
+++ Core/Api.Application/Features/MediatorDesignPattern/Queries/CastQueries/GetCastQuery.cs
@@ -0,0 +1,14 @@
+﻿using Api.Application.Features.MediatorDesignPattern.Results.CastResults;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Api.Application.Features.MediatorDesignPattern.Queries.CastQueries
+{
+    public class GetCastQuery:IRequest<List<GetCastQueryResult>>
+    {
+    }
+}
