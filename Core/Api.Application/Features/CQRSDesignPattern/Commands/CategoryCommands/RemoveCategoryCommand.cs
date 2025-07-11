8e8bfb4895ce2d238dc1f92834011d3fc556fde1
grbzemir <eyetkingurbuz@trakya.edu.tr>
2025-07-11 18:13:58 +0300

    Commands

---
+++ Core/Api.Application/Features/CQRSDesignPattern/Commands/CategoryCommands/RemoveCategoryCommand.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Api.Application.Features.CQRSDesignPattern.Commands.CategoryCommands
+{
+    public class RemoveCategoryCommand
+    {
+        public RemoveCategoryCommand(int categoryId)
+        {
+            CategoryId = categoryId;
+        }
+        public int CategoryId { get; set; }
+    }
+}
