717cc4ce28fadea68740b87dca7aac00372f802d
grbzemir <eyetkingurbuz@trakya.edu.tr>
2025-07-12 18:08:52 +0300

    Controllers

---
+++ Frontends/Api.WebUI/Controllers/UserWebUILayoutController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Api.WebUI.Controllers
+{
+    public class UserWebUILayoutController : Controller
+    {
+        public IActionResult LayoutUI()
+        {
+            return View();
+        }
+    }
+}
