b42f91ef51f01990794ad376dc2eb881aafd05a7
grbzemir <eyetkingurbuz@trakya.edu.tr>
2025-07-12 18:09:48 +0300

    WebUI

---
+++ Frontends/Api.WebUI/ViewComponents/UserLayoutWebUIViewComponents/_UserLayoutWebUILoginModalComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Api.WebUI.ViewComponents.UserLayoutWebUIViewComponents
+{
+	public class _UserLayoutWebUILoginModalComponentPartial:ViewComponent
+	{
+		public IViewComponentResult Invoke()
+		{
+			return View();
+		}
+	}
+}
