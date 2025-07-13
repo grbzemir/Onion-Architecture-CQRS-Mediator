34a77b424b0dd076dd83b1b9d7bc3c97da458242
grbzemir <eyetkingurbuz@trakya.edu.tr>
2025-07-13 19:17:53 +0300

    .

---
+++ Frontends/Api.WebUI/ViewComponents/UserLayoutWebUIViewComponents/_UserLayoutWebUIFooterComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Api.WebUI.ViewComponents.UserLayoutWebUIViewComponents
+{
+	public class _UserLayoutWebUIFooterComponentPartial:ViewComponent
+	{
+		public IViewComponentResult Invoke()
+		{
+			return View();
+		}
+	}
+}
